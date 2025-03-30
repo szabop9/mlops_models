4f55b6de6ba3dfb1ccfd8cc28719df268e1f081b
Peter Szabo <peter.szabo@student.hogent.be>
2025-03-30 17:40:09 +0200

    implement art

--- Airflow/dags/initial_training_dag.py
+++ Airflow/dags/initial_training_dag.py
@@ -180,7 +180,7 @@ with DAG("train_base_model_ec2", default_args=default_args, schedule_interval=No
     trigger_train_dag = TriggerDagRunOperator(
         task_id="train_art",
         trigger_dag_id="train_defence_models_ec2",
-        wait_for_completion=True,
+        wait_for_completion=False,
         conf={
             "model_name": "{{ ti.xcom_pull(task_ids='train_model', key='model_file') }}"
         },
